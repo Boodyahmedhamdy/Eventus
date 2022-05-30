63e55eebc76d37e39ce4ab133f800ffcddcd8dd9
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-05-30 14:20:21 +0200

    locator of the Event

---
+++ app/src/main/java/com/example/app/NavigationSystem/EventLocator.java
@@ -0,0 +1,10 @@
+package com.example.app.NavigationSystem;
+
+/**
+ * <h1>responsible for locating the Event suitable gate to
+ * enter from --> to avoid crowd</h1>
+ *
+ *
+ */
+public class EventLocator {
+}
