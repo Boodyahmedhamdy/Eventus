573aeeb1f03cc644cd4aeaaf72529b564bd32a0c
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-05-30 14:20:48 +0200

    locator of the customer-- like using google maps

---
+++ app/src/main/java/com/example/app/NavigationSystem/CustomerLocator.java
@@ -0,0 +1,13 @@
+package com.example.app.NavigationSystem;
+
+
+/**
+ * <h1>responsible for locating customers -- most commonly used by
+ * project manager to see if some customers are away from him
+ * <br><br>
+ * also used by parents to let them know where are there children</h1>
+ *
+ *
+ */
+public class CustomerLocator {
+}
