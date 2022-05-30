a4e63ac4ea80ad79568156b293d713134bf8c4bb
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-05-30 14:21:11 +0200

    manager locatoer

---
+++ app/src/main/java/com/example/app/NavigationSystem/ProjectManagerLocator.java
@@ -0,0 +1,10 @@
+package com.example.app.NavigationSystem;
+
+/**
+ * <h1>responsible for locating the project manager if some
+ * custormers needed him </h1>
+ *
+ *
+ */
+public class ProjectManagerLocator {
+}
