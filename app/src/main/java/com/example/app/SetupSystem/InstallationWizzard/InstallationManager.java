0f91529365e62699a09215b065e439b840e32e18
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-05-30 14:22:45 +0200

    installation manager

---
+++ app/src/main/java/com/example/app/SetupSystem/InstallationWizzard/InstallationManager.java
@@ -0,0 +1,11 @@
+package com.example.app.SetupSystem.InstallationWizzard;
+
+/**
+ * <h1>
+ *     the manager of installing the application
+ * </h1>
+ *
+ *
+ */
+public class InstallationManager {
+}
